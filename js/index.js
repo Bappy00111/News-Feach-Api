5e9ed8dbbd2cf8d7f3a48899fb010a0a76336e74
“[bappy <[bappymodok0011@gamil.com]>
2023-07-22 22:48:15 -0700

    alart section added

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ const showCataGores = (data) =>{
     // all catagores link 
     const catagoresContainer = document.getElementById('catagores-container');
     data.forEach(singleData => {
-        console.log(singleData.category_name)
+        // console.log(singleData.category_id)
         catagoresContainer.innerHTML += `
-        <a class="nav-link" href="#">${singleData.category_name}</a>
+        <a class="nav-link" href="#" onclick="fetchAllCatagore('${singleData.category_id}','${singleData.category_name}')">${singleData.category_name}</a>
         `
         // const div = document.createElement('div')
         // div.innerHTML = `
@@ -21,5 +21,24 @@ const showCataGores = (data) =>{
         
     });
     
+}
+
+// fetch all news catagore 
+
+const fetchAllCatagore = (category_id,category_name) =>{
+   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+   fetch(url)
+   .then(res => res.json())
+   .then(data => fetchAllShowCatagores(data.data,category_name))
+}
+
+const fetchAllShowCatagores = (datas,category_name) =>{
+    console.log(datas)
+    document.getElementById('news-counts').innerText = datas.length;
+    document.getElementById('catagore-names').innerText = category_name;
+
+    // datas.forEach(data =>{
+     
+    // })
 }
 // fechCatagore()
